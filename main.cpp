46396ccd8412c7355a02d25a5c52f026362079cf
Rafeeq Muhammad <RafeeqNMuhammad@gmail.com>
2025-06-10 18:31:41 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int function() {
+        return 0;
+    }
+};
+
+int main() {
+    Solution sol;
+    cout << sol.function() << endl;
+    return 0;
+}
+
