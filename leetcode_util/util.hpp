14bdf7e1b0d77905d13308e0a94437f59b4858bd
Rafeeq Muhammad <RafeeqNMuhammad@gmail.com>
2025-11-04 22:43:50 -0800

    Min()

--- leetcode_util/util.hpp
+++ leetcode_util/util.hpp
@@ -21,6 +21,19 @@ constexpr auto Max(T value, Ts... values) -> std::common_type_t<T, Ts...> {
     return result;
 }
 
+template <typename T>
+constexpr T Min(T value) {
+    return value;
+}
+
+template <typename T, typename... Ts>
+constexpr auto Min(T value, Ts... values) -> std::common_type_t<T, Ts...> {
+    using Result = std::common_type_t<T, Ts...>;
+    Result result = static_cast<Result>(value);
+    ((result = result > static_cast<Result>(values) ? static_cast<Result>(values) : result), ...);
+    return result;
+}
+
 }  // namespace leetcode_util
 
 namespace leetcode_util::detail {
